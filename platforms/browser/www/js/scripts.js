534f3ccf0b6074d7ada726cffa2817448367058f
exezamar <exe.zamar@gmail.com>
2017-07-30 20:26:25 -0300

    mejoras productos

--- platforms/browser/www/js/scripts.js
+++ platforms/browser/www/js/scripts.js
@@ -1,71 +1,69 @@
 
 var db = null;
 var cuenta = 0;
-
+var idTour = '';
+var nombreTour = '';
+var ubicacionTour = '';
 document.addEventListener('deviceready', function()
 {
-   
+
 	//CREAR BASE SI NO EXISTE
-	db = window.sqlitePlugin.openDatabase({name: 'brokersiOs.db', iosDatabaseLocation: 'Library'});
+	db = window.sqlitePlugin.openDatabase({name: 'brokersDev01.db', iosDatabaseLocation: 'Library'});
 	db.transaction(function (tx) {
-        tx.executeSql("CREATE TABLE IF NOT EXISTS tours (id INTEGER PRIMARY KEY, nombre text, ubicacion text, moneda text, fecha text)");
-    }, function (err) {
-        alert("An error occurred while initializing the app");
-    });
-
-
-  }, function(error) {
-    alert('SELECT error: ' + error.message);
+    tx.executeSql("CREATE TABLE IF NOT EXISTS tours (id INTEGER PRIMARY KEY, nombre text, ubicacion text, moneda text, fecha text)");
+    tx.executeSql("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY,idTour text, nombre text, precioUnidad text,precioCantidad, descripcion text,cantidadMinima text, CBM text, QTY text, tienda text, foto1 text, foto2 text, foto3 text, cantidadComprada text)");
+  }, function (err) {
+    alert("An error occurred while initializing the app");
   });
+
 });//fin deviceready
 
- $("#btnPopular").click(function(){
-      
- 	   //INGRESAR NUEVO REGISTRO
-       db.transaction(function(tx)
-      {
-		  tx.executeSql('INSERT INTO tours (nombre, ubicacion, moneda, fecha) VALUES (?,?,?,?)', [ 'nombre','ubicacion','moneda','fecha']);
-		  }, function(error) {
-		    alert('Transaction ERROR: ' + error.message);
-		  }, function() {
-		    alert('Populated database OK');
-		});//fin transaccion
+$("#btnPopular").click(function(){
 
-       //MOSTAR CUANTOS RECORDS HAY
-      	db.transaction(function(tx) {
-	    	 tx.executeSql('SELECT count(*) AS mycount FROM tours', [], function(tx, rs) {
-	     	 alert('Record count: ' + rs.rows.item(0).mycount);
-	   		 }, function(tx, error) {
-	     		 alert('SELECT error: ' + error.message);
-	    	});
-  		});//fin transaccion
+ 	   //INGRESAR NUEVO REGISTRO
+      db.transaction(function(tx)
+      {
+        tx.executeSql('INSERT INTO tours (nombre, ubicacion, moneda, fecha) VALUES (?,?,?,?)', [ 'nombre','ubicacion','moneda','fecha']);
+      }, function(error) {
+        alert('Transaction ERROR: ' + error.message);
+      }, function() {
+       
+		  });//fin transaccion
+
+      //MOSTAR CUANTOS RECORDS HAY
+      db.transaction(function(tx)
+      {
+       tx.executeSql('SELECT count(*) AS mycount FROM tours', [], function(tx, rs) {
+        alert('Record count: ' + rs.rows.item(0).mycount);
+      }, function(tx, error) {
+       alert('SELECT error: ' + error.message);
+     });
+  		  });//fin transaccion
  });//fin btnPopular
 
 
 
 function ocultarSlide(nombre){
-    $("#"+nombre).removeClass('bounceInLeft');
-    $("#"+nombre).addClass('bounceOutLeft');
-    setTimeout(function () {
-      $("#"+nombre).addClass('oculto');
-    }, 500);
+  $("#"+nombre).removeClass('bounceInLeft');
+  $("#"+nombre).addClass('bounceOutLeft');
+  setTimeout(function () {
+    $("#"+nombre).addClass('oculto');
+  }, 500);
 }
 function mostrarSlide(nombre){
-    $("#"+nombre).removeClass('bounceOutLeft');
-    $("#"+nombre).removeClass('oculto');
-    $("#"+nombre).addClass('bounceInRight');
+  $("#"+nombre).removeClass('bounceOutLeft');
+  $("#"+nombre).removeClass('oculto');
+  $("#"+nombre).addClass('bounceInRight');
 }
 
-function ofuscar(div){
-  $("#"+div).addClass('ofuscado');
-}
+function ofuscar(div){ $("#"+div).addClass('ofuscado');}
 
 $(document).ready( function() {
-    $(".ui-loader").hide();
-    function onSuccess(imageData){
-      var src = 'data:image/jpg;base64, ';
-      $("#image").attr('src', src+imageData);
-      $("#image").attr('src', imageData);
+  $(".ui-loader").hide();
+  function onSuccess(imageData){
+    var src = 'data:image/jpg;base64, ';
+    $("#image").attr('src', src+imageData);
+    $("#image").attr('src', imageData);
 
       // alert(src+imageData);
     }
@@ -81,77 +79,225 @@ $(document).ready( function() {
         }
 
 
-$("#bsFoto").click(function(){
-      navigator.camera.getPicture(onSuccess, onFail,
-      {
-          destinationType: Camera.DestinationType.FILE_URI,
-          sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-          popoverOptions: new CameraPopoverOptions(300, 300, 100, 100, Camera.PopoverArrowDirection.ARROW_ANY)
-      });
-});
+        $("#bsFoto").click(function(){
+          navigator.camera.getPicture(onSuccess, onFail,
+          {
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            popoverOptions: new CameraPopoverOptions(300, 300, 100, 100, Camera.PopoverArrowDirection.ARROW_ANY)
+          });
+        });
 
-$("#login").click(function(){
+        $("#login").click(function(){
 
-    ocultarSlide("cont-login");
-    setTimeout(function () {
-      mostrarSlide('cont-menu')
-    }, 500);
+          ocultarSlide("cont-login");
+          setTimeout(function () {
+            mostrarSlide('cont-menu')
+          }, 500);
 
-});
+        });
+        var tablaTours = '';
+        $("#bmTours").click(function(){
+          ocultarSlide("cont-menu");
+          setTimeout(function () {
+            mostrarSlide('cont-tours');
+            $("div.dataTables_scroll").css({"clear":"both !important"});
 
-$("#bmTours").click(function(){
-    ocultarSlide("cont-menu");
-    setTimeout(function () {
-      mostrarSlide('cont-tours')
-    }, 500);
-    //CREAR TABLA PARA TOURS
-    db.executeSql("SELECT * FROM Tours order by id desc", [], function (resultSet) {
-    var count = resultSet.rows.length;
-    for (var i = 0; i < resultSet.rows.length; i++)
-    {
-      var id = resultSet.rows.item(i).id;
-      var ubicacion = resultSet.rows.item(i).ubicacion;
-      var nombre = resultSet.rows.item(i).nombre;
-      var moneda = resultSet.rows.item(i).moneda;
-      var fecha = resultSet.rows.item(i).fecha;
-      $("#bodyTours").append("<tr idTour='"+id+"'><td style='color:black !important;font-size:1em;'>"+nombre+"</td><td>"+ubicacion+"</td><td>"+fecha+"</td></tr>")
-    };
-    
-        $('#tablaTours').DataTable( 
+          }, 500);
+      //CREAR TABLA PARA TOURS
+      db.executeSql("SELECT * FROM Tours order by id desc", [], function (resultSet) {
+        var count = resultSet.rows.length;
+        for (var i = 0; i < resultSet.rows.length; i++)
         {
-           order:[],
-           "paging": false,
-           "initComplete": function(settings, json) {},
-           "scrollY":        "250px",
-
+          var id = resultSet.rows.item(i).id;
+          
+          var ubicacion = resultSet.rows.item(i).ubicacion;
+          var nombre = resultSet.rows.item(i).nombre;
+          var moneda = resultSet.rows.item(i).moneda;
+          var fecha = resultSet.rows.item(i).fecha;
+          $("#bodyTours").append("<tr idTour='"+id+"'><td style='color:black !important;font-size:1em;'>"+nombre+"</td><td>"+ubicacion+"</td><td>"+fecha+"</td></tr>")
+        };
+        for (var i = 0; i < resultSet.rows.length; i++)
+        {
+          var id = resultSet.rows.item(i).id;
+          var ubicacion = resultSet.rows.item(i).ubicacion;
+          var nombre = resultSet.rows.item(i).nombre;
+          var moneda = resultSet.rows.item(i).moneda;
+          var fecha = resultSet.rows.item(i).fecha;
+          $("#bodyTours").append("<tr idTour='"+id+"'><td style='color:black !important;font-size:1em;'>"+nombre+"</td><td>"+ubicacion+"</td><td>"+fecha+"</td></tr>")
+        };
+        for (var i = 0; i < resultSet.rows.length; i++)
+        {
+          var id = resultSet.rows.item(i).id;
+          var ubicacion = resultSet.rows.item(i).ubicacion;
+          var nombre = resultSet.rows.item(i).nombre;
+          var moneda = resultSet.rows.item(i).moneda;
+          var fecha = resultSet.rows.item(i).fecha;
+          $("#bodyTours").append("<tr idTour='"+id+"'><td style='color:black !important;font-size:1em;'>"+nombre+"</td><td>"+ubicacion+"</td><td>"+fecha+"</td></tr>")
+        };
+        tablaTours = $('#tablaTours').DataTable( 
+        {
+         order:[],
+         "paging": false,
+         "initComplete": function(settings, json) {
+         },
+         "scrollY": "250px"
+         
        });
-        $(".dataTables_scrollHeadInner").css({"width":"100%"});
 
+        tablaTours.DataTable().columns.adjust().draw(); 
+      }, function(error) {
+       alert('SELECT error: ' + error.message);
+      });//fin query
 
   });//fin btnTours
+
 $("#bntour").click(function(){
-  
+
 	ofuscar('contTodoTours');
-
-$('#modal-ntour').removeClass('oculto');
-
-  });
+  setTimeout(function () {
+    mostrarSlide('modal-ntour');
+  }, 200);
+});
 
 $("#bcmntour").click(function(){
 	ocultarSlide('modal-ntour');
-	setTimeout(function () {
-    	mostrarSlide('cont-tours');
-		$(".inputmodal").each(function(){
-		 $(this).val(''); 
-		});
-    }, 200);
+	setTimeout(function ()
+  {
+    mostrarSlide('cont-tours');
+    $('#contTodoTours').removeClass('ofuscado');
+    $(".inputmodal").each(function()
+    {
+      $(this).val(''); 
+    });
+  }, 100);
 
 });
+$("#btnCrearTour").click(function(){
+  var nombre = $("#nnuevtour").val();
+  var ubicacion = $("#ubinuevtour").val();
+  var fecha = $("#fechnuevtour").val();
+  var moneda = $( "#inputMoneda option:selected" ).text();
+      //INGRESAR NUEVO REGISTRO
+      db.transaction(function(tx)
+      {
+        tx.executeSql('INSERT INTO tours (nombre, ubicacion, moneda, fecha) VALUES (?,?,?,?)', [ nombre, ubicacion, moneda, fecha]);
+      }, function(error) {
+        alert('Transaction ERROR: ' + error.message);
+      }, function() {
+        //guardado exitosamente
+        //ToDo: agregar el nuevo registro a la datatable.
+        ocultarSlide('modal-ntour');
+        setTimeout(function () {
+          mostrarSlide('cont-tours');
+          $('#contTodoTours').removeClass('ofuscado');
+          $(".inputmodal").each(function(){
+            $(this).val(''); 
+          });
+        }, 100);
+      });//fin transaccion
 
-  $(document).on('click','#tablaTours > tbody > tr > td', function() {
-          var padre = $(this).parent();
-          alert(padre.attr('idTour'));
-  });//fin eliminarInsumo
+    });
+$(document).on('click','#tablaTours > tbody > tr', function() {
+   
+   idTour = $(this).attr('idTour');
+   
+   nombreTour = $(this).children('td').eq(0).text();
+   ubicacionTour = $(this).children('td').eq(1).text();
+   var query = 'SELECT * FROM Products where "idTour" = "'+idTour+'" order by id desc';
+
+   db.executeSql(query, [], function (resultSet) {
+        var count = resultSet.rows.length;
+        // $("#contTodosProdu").empty();
+        
 
 
+        for (var i = 0; i < resultSet.rows.length; i++)
+        {
+          var id = resultSet.rows.item(i).id;
+          var nombre = resultSet.rows.item(i).nombre;
+          var tienda = resultSet.rows.item(i).tienda;
+          var precioUnidad = resultSet.rows.item(i).precioUnidad;
+          var precioCantidad = resultSet.rows.item(i).precioCantidad;
+
+
+
+          $("#contTodosProdu").append("<div id=prod"+i+ " class='contPrduct' idProduct="+id+"></div>");  
+          $("#prod"+i).append("<div class='contImagenProd'><img src='img/sinFoto.png' class='imgProduct'> </div>");
+          $("#prod"+i).append("<div class='contDescProd'><div class='contInfoProd'> <div class='posnomprod'><span>"+nombre+"</span> </div><div class='postienda'><span>Store ID: </span><br><span>"+tienda+"</span></div></div><div class='contInfoProd'><div class='contUnitPrice'><span>Unit price: <br>"+precioUnidad+"</span></div><div class='contBulkPrice'> <span>Bulk price: <br> "+precioCantidad+"</span></div></div>");
+
+          $("#prod"+i).append("<div class='posarrow'><div> <img src='img/arrow-right.svg' class='parrow-right'></div></div>");
+
+
+
+
+
+          // var ubicacion = resultSet.rows.item(i).ubicacion;
+          // var nombre = resultSet.rows.item(i).nombre;
+          // var moneda = resultSet.rows.item(i).moneda;
+          // var fecha = resultSet.rows.item(i).fecha;
+          // $("#bodyTours").append("<tr idTour='"+id+"'><td style='color:black !important;font-size:1em;'>"+nombre+"</td><td>"+ubicacion+"</td><td>"+fecha+"</td></tr>")
+        };
+      });
+   $("#tnombtour").text(nombreTour);
+   $("#tubictour").text(ubicacionTour);
+   $("#tnombtour2").text(nombreTour);
+   $("#tubictour2").text(ubicacionTour);
+  ocultarSlide('cont-tours');
+  setTimeout(function () {
+          mostrarSlide('cont-products');
+          
+        }, 200);
+  });//fin clickTablaTours
+  $("#btnVolverTour").click(function(){
+    ocultarSlide('cont-products');
+    setTimeout(function () {
+          mostrarSlide('cont-tours');
+        }, 200);
+  });
+
+  $("#contNewProduct").click(function(){
+    ocultarSlide('contTodoProductos');
+
+    setTimeout(function () {
+          mostrarSlide('modal-nproducto');
+        }, 200);
+
+  });
+  $("#btnVolverNProd").click(function(){
+    ocultarSlide('modal-nproducto');
+    setTimeout(function () {
+          mostrarSlide('contTodoProductos');
+        }, 200);
+  });
+  $("#btnCrearProducto").click(function(){
+
+      var nombreProd = $("#nnuevprod").val();
+      var descripcion = $("#descnuevprod").val();
+      var tienda = $("#storenuevprod").val();
+      var qty = $("#qtynuevprod").val();
+      var cbm = $("#cbmnuevprod").val();
+      var precioUnidad = $("#upricenuevprod").val();
+      var precioCantidad = $("#bpricenuevprod").val();
+      var minima = $("#minamnuevprod").val();
+      var cantidadComprada = $("#cantcompranuevprod").val();
+
+      alert(idTour+" | "+ nombreProd+" | "+ precioUnidad+" | "+ precioCantidad+" | "+descripcion+" | "+minima+" | "+cbm+" | "+qty+" | "+tienda+" | "+cantidadComprada);
+      // var foto1 = $("#").val();
+      // var foto2 = $("#").val();
+      // var foto3 = $("#").val();
+      alert('creando con idtour'+idTour);
+      //INGRESAR NUEVO REGISTRO
+        db.transaction(function(tx)
+        {
+          tx.executeSql('INSERT INTO products (idTour, nombre, precioUnidad, precioCantidad, descripcion,cantidadMinima,CBM,QTY,tienda, cantidadComprada ) VALUES (?,?,?,?,?,?,?,?,?,?)', [ idTour, nombreProd, precioUnidad, precioCantidad,descripcion,minima,cbm,qty,tienda,cantidadComprada]);
+        }, function(error) {
+          alert('Transaction ERROR: ' + error.message);
+        }, function() {
+          alert('producto agregado exitosamente al tour '+idTour);
+
+          $(".inputmodal").text('');
+        });//fin transaccion
+
+      });
 });//fin onready
--- www/js/scripts.js
+++ www/js/scripts.js
@@ -8,10 +8,10 @@ document.addEventListener('deviceready', function()
 {
 
 	//CREAR BASE SI NO EXISTE
-	db = window.sqlitePlugin.openDatabase({name: 'brokersDev00.db', iosDatabaseLocation: 'Library'});
+	db = window.sqlitePlugin.openDatabase({name: 'brokersDev01.db', iosDatabaseLocation: 'Library'});
 	db.transaction(function (tx) {
     tx.executeSql("CREATE TABLE IF NOT EXISTS tours (id INTEGER PRIMARY KEY, nombre text, ubicacion text, moneda text, fecha text)");
-    tx.executeSql("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY,idTour text, nombre text, precioUnidad text,precioCantidad, descripcion text,cantidadMinima text, CBM text, QTY text, tienda text, foto1 text, foto2 text, foto3 text)");
+    tx.executeSql("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY,idTour text, nombre text, precioUnidad text,precioCantidad, descripcion text,cantidadMinima text, CBM text, QTY text, tienda text, foto1 text, foto2 text, foto3 text, cantidadComprada text)");
   }, function (err) {
     alert("An error occurred while initializing the app");
   });
@@ -27,7 +27,7 @@ $("#btnPopular").click(function(){
       }, function(error) {
         alert('Transaction ERROR: ' + error.message);
       }, function() {
-        alert('Populated database OK');
+       
 		  });//fin transaccion
 
       //MOSTAR CUANTOS RECORDS HAY
@@ -110,6 +110,7 @@ $(document).ready( function() {
         for (var i = 0; i < resultSet.rows.length; i++)
         {
           var id = resultSet.rows.item(i).id;
+          
           var ubicacion = resultSet.rows.item(i).ubicacion;
           var nombre = resultSet.rows.item(i).nombre;
           var moneda = resultSet.rows.item(i).moneda;
@@ -200,10 +201,48 @@ $("#btnCrearTour").click(function(){
 $(document).on('click','#tablaTours > tbody > tr', function() {
    
    idTour = $(this).attr('idTour');
+   
    nombreTour = $(this).children('td').eq(0).text();
    ubicacionTour = $(this).children('td').eq(1).text();
+   var query = 'SELECT * FROM Products where "idTour" = "'+idTour+'" order by id desc';
+
+   db.executeSql(query, [], function (resultSet) {
+        var count = resultSet.rows.length;
+        // $("#contTodosProdu").empty();
+        
+
+
+        for (var i = 0; i < resultSet.rows.length; i++)
+        {
+          var id = resultSet.rows.item(i).id;
+          var nombre = resultSet.rows.item(i).nombre;
+          var tienda = resultSet.rows.item(i).tienda;
+          var precioUnidad = resultSet.rows.item(i).precioUnidad;
+          var precioCantidad = resultSet.rows.item(i).precioCantidad;
+
+
+
+          $("#contTodosProdu").append("<div id=prod"+i+ " class='contPrduct' idProduct="+id+"></div>");  
+          $("#prod"+i).append("<div class='contImagenProd'><img src='img/sinFoto.png' class='imgProduct'> </div>");
+          $("#prod"+i).append("<div class='contDescProd'><div class='contInfoProd'> <div class='posnomprod'><span>"+nombre+"</span> </div><div class='postienda'><span>Store ID: </span><br><span>"+tienda+"</span></div></div><div class='contInfoProd'><div class='contUnitPrice'><span>Unit price: <br>"+precioUnidad+"</span></div><div class='contBulkPrice'> <span>Bulk price: <br> "+precioCantidad+"</span></div></div>");
+
+          $("#prod"+i).append("<div class='posarrow'><div> <img src='img/arrow-right.svg' class='parrow-right'></div></div>");
+
+
+
+
+
+          // var ubicacion = resultSet.rows.item(i).ubicacion;
+          // var nombre = resultSet.rows.item(i).nombre;
+          // var moneda = resultSet.rows.item(i).moneda;
+          // var fecha = resultSet.rows.item(i).fecha;
+          // $("#bodyTours").append("<tr idTour='"+id+"'><td style='color:black !important;font-size:1em;'>"+nombre+"</td><td>"+ubicacion+"</td><td>"+fecha+"</td></tr>")
+        };
+      });
    $("#tnombtour").text(nombreTour);
    $("#tubictour").text(ubicacionTour);
+   $("#tnombtour2").text(nombreTour);
+   $("#tubictour2").text(ubicacionTour);
   ocultarSlide('cont-tours');
   setTimeout(function () {
           mostrarSlide('cont-products');
@@ -231,5 +270,34 @@ $(document).on('click','#tablaTours > tbody > tr', function() {
           mostrarSlide('contTodoProductos');
         }, 200);
   });
+  $("#btnCrearProducto").click(function(){
 
+      var nombreProd = $("#nnuevprod").val();
+      var descripcion = $("#descnuevprod").val();
+      var tienda = $("#storenuevprod").val();
+      var qty = $("#qtynuevprod").val();
+      var cbm = $("#cbmnuevprod").val();
+      var precioUnidad = $("#upricenuevprod").val();
+      var precioCantidad = $("#bpricenuevprod").val();
+      var minima = $("#minamnuevprod").val();
+      var cantidadComprada = $("#cantcompranuevprod").val();
+
+      alert(idTour+" | "+ nombreProd+" | "+ precioUnidad+" | "+ precioCantidad+" | "+descripcion+" | "+minima+" | "+cbm+" | "+qty+" | "+tienda+" | "+cantidadComprada);
+      // var foto1 = $("#").val();
+      // var foto2 = $("#").val();
+      // var foto3 = $("#").val();
+      alert('creando con idtour'+idTour);
+      //INGRESAR NUEVO REGISTRO
+        db.transaction(function(tx)
+        {
+          tx.executeSql('INSERT INTO products (idTour, nombre, precioUnidad, precioCantidad, descripcion,cantidadMinima,CBM,QTY,tienda, cantidadComprada ) VALUES (?,?,?,?,?,?,?,?,?,?)', [ idTour, nombreProd, precioUnidad, precioCantidad,descripcion,minima,cbm,qty,tienda,cantidadComprada]);
+        }, function(error) {
+          alert('Transaction ERROR: ' + error.message);
+        }, function() {
+          alert('producto agregado exitosamente al tour '+idTour);
+
+          $(".inputmodal").text('');
+        });//fin transaccion
+
+      });
 });//fin onready
