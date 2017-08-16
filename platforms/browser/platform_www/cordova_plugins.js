be3baa511ca007990d58633a6fc28ab2080eb90e
exezamar <exe.zamar@gmail.com>
2017-08-15 23:07:11 -0300

    sin whitelist

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -44,8 +44,7 @@ module.exports.metadata =
 {
     "cordova-sqlite-storage": "2.0.4",
     "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-camera": "2.4.1",
-    "cordova-plugin-whitelist": "1.3.2"
+    "cordova-plugin-camera": "2.4.1"
 }
 // BOTTOM OF METADATA
 });
